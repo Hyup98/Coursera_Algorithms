9e500526dd53484570a10256141b4119d9664aec
Hyup <kkddong98@gmail.com>
2021-08-20 08:45:00 +0900

    no message

---
+++ Percolation/src/PercolationStats.java
@@ -0,0 +1,21 @@
+public class PercolationStats {
+
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials)
+
+    // sample mean of percolation threshold
+    public double mean()
+
+    // sample standard deviation of percolation threshold
+    public double stddev()
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo()
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi()
+
+    // test client (see below)
+    public static void main(String[] args)
+
+}
