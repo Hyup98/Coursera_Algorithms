7cf7585d82b180952326fdac5c931df35b96035a
Hyup <kkddong98@gmail.com>
2021-09-03 23:15:29 +0900

    no message

--- CollinearPoints/src/BruteCollinearPoints.java
+++ CollinearPoints/src/BruteCollinearPoints.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class BruteCollinearPoints {
+public class BruteCollinearPoints {
+    private Point[] points;
+
+    // finds all line segments containing 4 points
+    public BruteCollinearPoints(Point[] points) {
+        this.points = new Point[4];
+        this.points = points;
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+
+    }
 }
--- CollinearPoints/src/FastCollinearPoints.java
+++ CollinearPoints/src/FastCollinearPoints.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class FastCollinearPoints {
+public class FastCollinearPoints {
+    public FastCollinearPoints(Point[] points)     // finds all line segments containing 4 or more points
+    public           int numberOfSegments()        // the number of line segments
+    public LineSegment[] segments()                // the line segments
 }
